c2d37f0838ad2cbe4b6a74b1ebb661cef4cde5e2
Varun-Bhatia <varun.bhatia61@gmail.com>
2018-11-07 02:53:47 +0530

    Add files via upload

---
+++ UserAssetRequest.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    String cs = @"Data Source=(localdb)\MSSQLlocalDB;Initial Catalog=ITProject;Integrated Security=True";
+    int avb = 0;
+    String aname = "";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection(cs);
+        con.Open();
+        String qry = "SELECT * FROM [Assets] WHERE [AssetId]=" + Request.QueryString["AssetId"];
+        SqlCommand cmd = new SqlCommand(qry, con);
+        SqlDataReader dr = cmd.ExecuteReader();
+        dr.Read();
+        AssetName.Text = dr["AssetName"].ToString();
+        aname= dr["AssetName"].ToString();
+        AQuantity.Text = dr["AvailableQuantity"].ToString();
+        int.TryParse(dr["AvailableQuantity"].ToString(), out avb);
+        con.Close();
+    }
+
+        protected void Button1_Click(object sender, EventArgs e)
+    {
+        int req = 0;
+        int.TryParse(Quantity.Text, out req);
+        if (avb >= req)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand command = new SqlCommand("INSERT INTO [Requests] VALUES(@UserName,@AssetId,@Status,@DateofApproval,@QuantityRequested,@AssetName)", con);
+            command.Parameters.AddWithValue("@UserName", Session["UserName"].ToString());
+            command.Parameters.AddWithValue("@AssetId", Request.QueryString["AssetId"]);
+            command.Parameters.AddWithValue("@Status", "Pending");
+            DateTime dt = System.DateTime.Now;
+            command.Parameters.AddWithValue("@DateofApproval", dt);
+            command.Parameters.AddWithValue("@QuantityRequested", req);
+            command.Parameters.AddWithValue("@AssetName", aname);
+            command.ExecuteNonQuery();
+            command = new SqlCommand("UPDATE Assets SET AvailableQuantity = " + (avb-req) + " WHERE AssetId =\'" + Request.QueryString["AssetId"] + "\'", con);
+            command.ExecuteNonQuery();
+            con.Close();
+            Label1.Text = "Request Submitted Successfully";
+            Label1.Visible = true;
+        }
+        else
+        {
+            Label1.Text = "Quantity requested more than available";
+            Label1.Visible = true;
+        }
+    }
+}
---
+++ UserAssetView.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/UserMasterPage.master" AutoEventWireup="true" CodeFile="UserAssetView.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    Select required asset from table and click submit<br />
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" SelectCommand="SELECT * FROM [Assets]" ConnectionString="Data Source=(localdb)\MSSQLlocalDB;Initial Catalog=ITProject;Integrated Security=True"></asp:SqlDataSource>
+    <asp:GridView ID="GridView1" runat="server" DataSourceID="SqlDataSource1" DataKeyNames="AssetId" AutoGenerateColumns="false">
+        <Columns>
+            <asp:BoundField DataField="AssetName" HeaderText="Asset Name" />
+            <asp:BoundField DataField="TotalQuantity" HeaderText=" Total Quantity" />
+            <asp:BoundField DataField="AvailableQuantity" HeaderText="Available Quantity" />
+            <asp:CommandField ShowSelectButton="true" ButtonType="Button" />
+        </Columns>
+        <SelectedRowStyle Font-Bold="true" />
+    </asp:GridView>
+    <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click"/>
+</asp:Content>
+
